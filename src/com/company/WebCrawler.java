7b96bc99c11ae3984a1a8610934e5adca82c8e4c
hadyelzayady <hadyelzayady@yahoo.com>
2018-04-01 17:14:43 +0200

    cleaning crawler code

--- src/com/company/WebCrawler.java
+++ src/com/company/WebCrawler.java
@@ -13,21 +13,20 @@ import java.nio.charset.StandardCharsets;
 import java.security.NoSuchAlgorithmException;
 import java.util.*;
 import java.util.concurrent.atomic.AtomicLong;
-import java.util.regex.MatchResult;
-import java.util.stream.Collectors;
 import java.security.MessageDigest;
-import java.util.concurrent.atomic.AtomicInteger;
 
 //todo restart crawler after finishing
 //todo resume after interrupt
 public class WebCrawler implements Runnable {
 
-    DBController controller;
-    final int lowest_priority = 5;
-    final int crawler_limit = 5000;
-    static final AtomicLong number_crawled = new AtomicLong(0);
+    /**
+     *
+     */
+    private DBController controller;
+    private final int crawler_limit = 5000;
+    private static final AtomicLong number_crawled = new AtomicLong(0);
 
-    public WebCrawler() {
+    WebCrawler() {
         controller = DBController.ContollerInit();
         long visited_count = controller.getVisitedCount();
         if (0 < visited_count && visited_count < crawler_limit)//interrupt happened
@@ -40,6 +39,7 @@ public class WebCrawler implements Runnable {
             number_crawled.setPlain(0);
         }
     }
+
     public void run() {
         org.bson.Document link_doc = controller.getLinkFromFrontierAndSetOnwork();
         String link = link_doc.getString("_id");
@@ -50,8 +50,6 @@ public class WebCrawler implements Runnable {
             try {
                 if (isPageAllowedToCrawl(link)) {
                     System.out.println(number_crawled);
-//                    BufferedReader page_buffer = downloadPage(link);
-//                    String page_content = getString(page_buffer);
                     Document page = Jsoup.connect(link).get();
                     String page_content = page.outerHtml();
                     String checksum = toHexString(calcChecksum(page_content));
@@ -88,6 +86,7 @@ public class WebCrawler implements Runnable {
         int link_priority = link_doc.getInteger("Priority");
         if (new_checksum.equals(old_checksum))//not change --> lower priority (higher number is lower priority:1 is highest priority and 5 is lowest
         {
+            int lowest_priority = 5;
             controller.setPriority((link_priority % lowest_priority) + 1, link_doc.getString("_id"));//lower
         } else//changed
         {
@@ -104,65 +103,27 @@ public class WebCrawler implements Runnable {
         }
     }
 
-    private String getString(BufferedReader page_buffer) {
-        String line = null;
-        StringBuilder sb = new StringBuilder();
-        try {
-            while ((line = page_buffer.readLine()) != null) {
-                sb.append(line + "\n");
-            }
-            return sb.toString();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    public boolean doesLinkExistInSeed(String link) {
-        return false;
-    }
-
-    public String normalizeLink(String url) {
-        String normalized_url=url.replaceAll("#\\S*","");
-        normalized_url=normalized_url.toLowerCase();
+    private String normalizeLink(String url) {
+        String normalized_url = url.replaceAll("#\\S*", "");
+        normalized_url = normalized_url.toLowerCase();
         return normalized_url;
     }
 
 
-    public List<String> getInnerLinks(Scanner content){
-        String pat = "(?i)(href)(\\s*)=\\s*(.+?)>"; //(?i)(<\s*a)(.+?)(href)(\s*)=\s*(.+?)> to look only in <a tags
-        return content.findAll(pat).map(MatchResult::group).collect(Collectors.toList());
-    }
-
-    public BufferedReader downloadPage(String url) throws IOException {
-        return new BufferedReader(new InputStreamReader((new URL(url)).openStream()));
-    }
-
-    public byte[] calcChecksum(String content) throws NoSuchAlgorithmException {
+    private byte[] calcChecksum(String content) throws NoSuchAlgorithmException {
         MessageDigest digest = MessageDigest.getInstance("SHA-256");
         return digest.digest(
                 content.getBytes(StandardCharsets.UTF_8));
     }
 
-    public boolean isPageDownloadedBefore(String check_sum) {
+    private boolean isPageDownloadedBefore(String check_sum) {
         //if there is url in visited table with check_sum = check_sum -->do nothong
         return controller.isUrlWithCheckSumInVisited(check_sum);
     }
 
-    public boolean isUrlInSeed(String url) {
-        //check url in seet table
-        return true;
-    }
 
-    public void addLinksToSeed(List<String> links) {
-        for (String link : links) {
-            String normalized_link = normalizeLink(link);
-            controller.addUrlToSeed(normalized_link);
-        }
-    }
-
-    public void addUrlToVisited(String url, String checksum) {
-        controller.addUrlToVisited(url,checksum);
+    private void addUrlToVisited(String url, String checksum) {
+        controller.addUrlToVisited(url, checksum);
     }
 
     public boolean isPageHtml(String url) {
@@ -178,8 +139,7 @@ public class WebCrawler implements Runnable {
     }
 
 
-
-    public void savePageInFile(String checksum, String page_string) throws IOException {
+    private void savePageInFile(String checksum, String page_string) throws IOException {
         String filename = "Pages/" + checksum + ".html";
         BufferedWriter writer = null;
         try {
@@ -193,7 +153,7 @@ public class WebCrawler implements Runnable {
         }
     }
 
-    public String toHexString(byte[] bytes) {
+    private String toHexString(byte[] bytes) {
         StringBuilder sb = new StringBuilder();
         for (byte b : bytes) {
             sb.append(String.format("%02X", b));
@@ -202,14 +162,13 @@ public class WebCrawler implements Runnable {
     }
 
     //todo normalize url in robots
-    public boolean isPageAllowedToCrawl(String url) {
+    private boolean isPageAllowedToCrawl(String url) {
         try {
             //todo if not robots set allow and disallow null
             URI uri = new URI(url);
             String host = uri.getHost();
             String path = uri.getPath();
             org.bson.Document robot_doc = controller.getRobot(host, path);
-//            Boolean s=robot_doc.getBoolean("updated");
             if (robot_doc != null && robot_doc.getBoolean("updated")) {
 
                 return isPathAllowedInRobot(path, robot_doc);
@@ -241,7 +200,7 @@ public class WebCrawler implements Runnable {
 
     }
 
-    public boolean addRobotAndCheckAllow(String text, String host, String checksum, String path) {
+    private boolean addRobotAndCheckAllow(String text, String host, String checksum, String path) {
         try {
             //text="";//todo test that
 
@@ -297,22 +256,21 @@ public class WebCrawler implements Runnable {
         return true;
     }
 
-    protected boolean isPathAllowedInRobot(String path, org.bson.Document robot) {
-        List<String> allow = (List<String>) robot.get("allow");
-        List<String> disallow = (List<String>) robot.get("disallow");
-        for (int i = 0; i < allow.size(); i++) {
-            final String allowed_path = allow.get(i);
-            if (path.matches(allowed_path))
-                return true;
+    private boolean isPathAllowedInRobot(String path, org.bson.Document robot) {
+        try {
+            List<String> allow = (List<String>) robot.get("allow");
+            List<String> disallow = (List<String>) robot.get("disallow");
+            for (final String allowed_path : allow) {
+                if (path.matches(allowed_path))
+                    return true;
+            }
+            for (final String disallowed_path : disallow) {
+                if (path.matches(disallowed_path))
+                    return false;
+            }
+            return true;
+        } catch (Exception ex) {
+            return true;
         }
-        for (int i = 0; i < disallow.size(); i++) {
-            final String disallowed_path = disallow.get(i);
-            ;
-            if (path.matches(disallowed_path))
-                return false;
-        }
-        return true;
     }
-
-
 }
